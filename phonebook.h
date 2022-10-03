53bd38e044f5d7fe44e82169cdc241ad6491911e
robotmeatsuit <64236644+Chargde-Porcupine@users.noreply.github.com>
2022-10-02 17:37:22 -0700

    finished up pb_write, cleaned up code a bit

--- phonebook.h
+++ phonebook.h
@@ -1,10 +1,20 @@
-
-#include <rmsutils.h> //utils!
-#include <jsmn.h>     //json
+#include <jsmn.h>   //json
+#include <stdio.h>  //inputs and outputs
+#include <string.h> //string functions like strcat() and strcpy()
+#include <malloc.h> //memory allocation
 
 #define INT_A 1306858826
 #define INT_B 1130263739
 #define INT_LE 2147483647
+//maybe generate new constants on pb_init?
+
+#define PB_GENERIC_SUCCESS 0
+#define PB_GENERIC_FAILURE 1
+#define JSON_PARSE_FAILURE 2
+#define PB_CREATE_FAILURE 3
+#define PB_WRITE_FAILURE 4
+#define PB_REMOVE_FAILURE 5
+#define PB_LOOKUP_FAILURE NULL
 
 /*
 stuff to do:
@@ -33,6 +43,29 @@ struct phonebook_entry
     struct arr_entry *data;
 };
 
+//utility function
+char *strs_cat(const char **strs)
+{
+    char *output = malloc(1 * sizeof(char));
+
+    size_t curr_len = 0;
+
+    for (int i = 0; strs[i][0] != '\0'; ++i)
+    {
+        curr_len += strlen(strs[i]);
+        output = realloc(output, (curr_len+1) * sizeof(char));
+        if (i == 0)
+        {
+            strcpy(output, strs[i]);
+            continue;
+        }
+        strcat(output, strs[i]);
+    }
+
+    return output;
+}
+
+
 unsigned int pb_hash(char *key, int size, int a, int b, int prime)
 {
     unsigned int sum = 0;
@@ -59,7 +92,8 @@ struct arr_entry *pb_lookup(char *key, struct phonebook_entry *holder)
                 return &holder[pos].data[i];
             }
         }
-        return NULL;
+        //*can change to an errorcode?
+        return PB_LOOKUP_FAILURE;
     }
 }
 
@@ -69,7 +103,7 @@ int pb_create(char *key, char *value, struct phonebook_entry *holder)
     holder[pos].hash = pos;
     if (!pb_lookup(key, holder))
     {
-        if (holder[pos].data)
+        if (holder[pos].datalen > 0)
         {
             holder[pos].data = realloc(holder[pos].data, (1 + holder[pos].datalen) * sizeof(struct arr_entry));
             holder[pos].data[holder[pos].datalen].key = strdup(key);
@@ -81,9 +115,9 @@ int pb_create(char *key, char *value, struct phonebook_entry *holder)
         holder[pos].datalen = 1;
         holder[pos].data[0].key = strdup(key);
         holder[pos].data[0].value = strdup(value);
-        return 0;
+        return PB_GENERIC_SUCCESS;
     }
-    return 1;
+    return PB_CREATE_FAILURE;
 }
 
 int pb_remove(char *key, struct phonebook_entry *holder)
@@ -99,6 +133,9 @@ int pb_remove(char *key, struct phonebook_entry *holder)
 
                 if (strcmp(holder[pos].data[i].key, key) == 0)
                 {
+                    free(holder[pos].data[i].key);
+                    free(holder[pos].data[i].value);
+                    free(&holder[pos].data[i]);
                     removemode = 1;
                     continue;
                 }
@@ -113,13 +150,21 @@ int pb_remove(char *key, struct phonebook_entry *holder)
     if (removemode == 1)
     {
         holder[pos].datalen--;
+        return PB_GENERIC_SUCCESS;
     }
+    return PB_REMOVE_FAILURE;
 }
 
-int pb_init(struct phonebook_entry *holder)
+int pb_init(struct phonebook_entry *holder, char *filename)
 {
-    static const char *JSON_STRING =
-        "{\"user\": \"johndoe\", \"admin\": false,\"admin\": true,  \"uid\": 1000}";
+    FILE *fp;
+    fp = fopen(filename, "r");
+    if(fseek(fp, 0L, SEEK_END) != 0){perror("seeking");return 1;};
+    int sz = ftell(fp);
+    char JSON_STRING[sz];
+    JSON_STRING[sz] = '\0';
+    rewind(fp);
+    fread(JSON_STRING, 1,sz,  (FILE *)fp);
     jsmn_parser parser;
     jsmn_init(&parser);
     int count = jsmn_parse(&parser, JSON_STRING, strlen(JSON_STRING), NULL, 0);
@@ -129,43 +174,50 @@ int pb_init(struct phonebook_entry *holder)
     if (obj < 0)
     {
         printf("Failed to parse JSON: %d\n", obj);
-        return 1;
+        return JSON_PARSE_FAILURE;
     }
     /* Assume the top-level element is an object */
     if (obj < 1 || tokens[0].type != JSMN_OBJECT)
     {
         printf("Failed to parse JSON: %d\n", parser);
         printf("Object expected\n");
-        return 1;
+        return JSON_PARSE_FAILURE;
     }
     for (int i = 1; i < obj; i += 2)
     {
         char *key = strndup(JSON_STRING + tokens[i].start, tokens[i].end - tokens[i].start);
         char *value = strndup(JSON_STRING + tokens[i + 1].start, tokens[i + 1].end - tokens[i + 1].start);
-        pb_create(key, value, holder);
+        if(pb_create(key, value, holder) == PB_CREATE_FAILURE) {return PB_CREATE_FAILURE;}
     }
+    return PB_GENERIC_SUCCESS;
 }
 
-int pb_write(struct phonebook_entry *holder)
+int pb_write(struct phonebook_entry *holder, char *filename)
 {
     char *JSON_STRING = malloc(sizeof(char) * 2);
     JSON_STRING = strdup("{");
     for (int i = 0; i < ENTRY_AMOUNT; i++)
     {
         int currlen = 0;
-        if (holder[i].datalen > 0)
-        {
             for (int j = 0; j < holder[i].datalen; j++)
             {
                 char *prepend = (strcmp(JSON_STRING, "{") == 0) ? "\"" : ",\"";
-                currlen += strlen("\"\"\"\":");
+                currlen += strlen(",\"\"\"\": ");
                 currlen += strlen(holder[i].data[j].key);
                 currlen += strlen(holder[i].data[j].value);
                 JSON_STRING = realloc(JSON_STRING, (currlen + 2) * sizeof(char));
-                JSON_STRING = strs_cat((const char *[]){JSON_STRING, prepend, holder[i].data[j].key, "\"", ":", "\"", holder[i].data[j].value, "\"", ""});
+                JSON_STRING = strs_cat((const char *[]){JSON_STRING, prepend, holder[i].data[j].key, "\"", ": ", "\"", holder[i].data[j].value, "\"", ""});
             }
-        }
     }
     JSON_STRING = strcat(JSON_STRING, "}");
-    //can now write to file
+    // can now write to file
+    FILE *fp = fopen(filename, "w");
+    if (fp)
+    {
+        fputs(JSON_STRING, fp);
+        fclose(fp);
+        free(JSON_STRING);
+        return PB_GENERIC_SUCCESS;
+    }
+    return PB_WRITE_FAILURE;
 }
